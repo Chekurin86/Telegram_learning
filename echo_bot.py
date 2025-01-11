a7598ab568ce438a5b622bdb08d842f0fd807ab6
Maxim Chekurin <makschekurin@gmail.com>
2025-01-11 06:02:22 +0300

    Добавлен эхо-бот

--- echo_bot.py
+++ echo_bot.py
@@ -2,31 +2,44 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from token_1 import TOKEN
-from aiogram import F
 
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
 BOT_TOKEN = TOKEN
 
+# Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command(commands=['start']))
-async def process_start_command(message: Message):
-    await message.answer("Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь")
 
-@dp.message(Command(commands=['help']))
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands='start'))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
-        "я пришлю тебе твое сообщение"
+        'я пришлю тебе твое сообщение'
     )
 
-@dp.message(F.photo)
-async def send_photo_echo(message: Message):
-    await message.reply_photo(message.photo[0].file_id)
 
-@dp.message(F.text)
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    print(message.model_dump_json(indent=4, exclude_none=True))
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
