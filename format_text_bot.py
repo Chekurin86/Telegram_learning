5a8e10759ea28fc7aafb465aafe4352aaecd5718
Maxim Chekurin <makschekurin@gmail.com>
2025-02-02 22:46:47 +0300

    Добавлен модуль с форматированием текста

--- format_text_bot.py
+++ format_text_bot.py
@@ -3,14 +3,12 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-from environs import Env
 
-env = Env()  # Создаем экземпляр класса Env
-env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+BOT_TOKEN = 'BOT TOKEN HERE'
 
-bot_token = env('BOT_TOKEN')
-
-bot = Bot(bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
 
@@ -18,13 +16,22 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        text='Привет!\n\nЯ бот, демонстрирующий '
-             'как работает HTML-разметка. Отправь команду '
-             'из списка ниже:\n\n'
+        text='Привет!\n\nЯ бот, демонстрирующий как работает '
+             'HTML-разметка. Отправь команду из списка ниже:\n\n'
              '/bold - жирный текст\n'
              '/italic - наклонный текст\n'
              '/underline - подчеркнутый текст\n'
-             '/spoiler - спойлер'
+             '/strike - зачеркнутый текст\n'
+             '/spoiler - спойлер\n'
+             '/link - внешняя ссылка\n'
+             '/tglink - внутренняя ссылка\n'
+             '/code - моноширинный текст\n'
+             '/pre - предварительно форматированный текст\n'
+             '/precode - предварительно форматированный блок кода\n'
+             '/precodediff - разница между &lt;code&gt; и &lt;pre&gt;\n'
+             '/boldi - жирный наклонный текст\n'
+             '/iu - наклонный подчеркнутый текст\n'
+             '/biu - жирный наклонный подчеркнутый текст'
     )
 
 
@@ -32,13 +39,22 @@ async def process_start_command(message: Message):
 @dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
-        text='Я бот, демонстрирующий '
-             'как работает HTML-разметка. Отправь команду '
-             'из списка ниже:\n\n'
+        text='Я бот, демонстрирующий как работает '
+             'HTML-разметка. Отправь команду из списка ниже:\n\n'
              '/bold - жирный текст\n'
              '/italic - наклонный текст\n'
              '/underline - подчеркнутый текст\n'
-             '/spoiler - спойлер'
+             '/strike - зачеркнутый текст\n'
+             '/spoiler - спойлер\n'
+             '/link - внешняя ссылка\n'
+             '/tglink - внутренняя ссылка\n'
+             '/code - моноширинный текст\n'
+             '/pre - предварительно форматированный текст\n'
+             '/precode - предварительно форматированный блок кода\n'
+             '/precodediff - разница между &lt;code&gt; и &lt;pre&gt;\n'
+             '/boldi - жирный наклонный текст\n'
+             '/iu - наклонный подчеркнутый текст\n'
+             '/biu - жирный наклонный подчеркнутый текст'
     )
 
 
@@ -46,11 +62,10 @@ async def process_help_command(message: Message):
 @dp.message(Command(commands='bold'))
 async def process_bold_command(message: Message):
     await message.answer(
-        text='<b>Это текст, демонстрирующий '
-             'как работает HTML-разметка, '
-             'делающая текст жирным.\n\n'
-             'Чтобы еще раз посмотреть список доступных команд - '
-             'отправь команду /help</b>'
+        text='&lt;b&gt;Это жирный текст&lt;/b&gt;:\n'
+             '<b>Это жирный текст</b>\n\n'
+             '&lt;strong&gt;И это тоже жирный текст&lt;/strong&gt;:\n'
+             '<strong>И это тоже жирный текст</strong>'
     )
 
 
@@ -58,11 +73,10 @@ async def process_bold_command(message: Message):
 @dp.message(Command(commands='italic'))
 async def process_italic_command(message: Message):
     await message.answer(
-        text='<i>Это текст, демонстрирующий '
-             'как работает HTML-разметка, '
-             'делающая текст наклонным.\n\n'
-             'Чтобы еще раз посмотреть список доступных команд - '
-             'отправь команду /help</i>'
+        text='&lt;i&gt;Это наклонный текст&lt;/i&gt;:\n'
+             '<i>Это наклонный текст</i>\n\n'
+             '&lt;em&gt;И это тоже наклонный текст&lt;/em&gt;:\n'
+             '<em>И это тоже наклонный текст</em>'
     )
 
 
@@ -70,11 +84,23 @@ async def process_italic_command(message: Message):
 @dp.message(Command(commands='underline'))
 async def process_underline_command(message: Message):
     await message.answer(
-        text='<u>Это текст, демонстрирующий '
-             'как работает HTML-разметка, '
-             'делающая текст подчеркнутым.\n\n'
-             'Чтобы еще раз посмотреть список доступных команд - '
-             'отправь команду /help</u>'
+        text='&lt;u&gt;Это подчеркнутый текст&lt;/u&gt;:\n'
+             '<u>Это подчеркнутый текст</u>\n\n'
+             '&lt;ins&gt;И это тоже подчеркнутый текст&lt;/ins&gt;:\n'
+             '<ins>И это тоже подчеркнутый текст</ins>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/strike"
+@dp.message(Command(commands='strike'))
+async def process_strike_command(message: Message):
+    await message.answer(
+        text='&lt;s&gt;Это зачеркнутый текст&lt;/s&gt;:\n'
+             '<s>Это зачеркнутый текст</s>\n\n'
+             '&lt;strike&gt;И это зачеркнутый текст&lt;/strike&gt;:\n'
+             '<strike>И это зачеркнутый текст</strike>\n\n'
+             '&lt;del&gt;И это тоже зачеркнутый текст&lt;/del&gt;:\n'
+             '<del>И это тоже зачеркнутый текст</del>'
     )
 
 
@@ -82,17 +108,117 @@ async def process_underline_command(message: Message):
 @dp.message(Command(commands='spoiler'))
 async def process_spoiler_command(message: Message):
     await message.answer(
-        text='<tg-spoiler>Это текст, демонстрирующий '
-             'как работает HTML-разметка, '
-             'убирающая текст под спойлер.\n\n'
-             'Чтобы еще раз посмотреть список доступных команд - '
-             'отправь команду /help</tg-spoiler>'
+        text='&lt;span class="tg-spoiler"&gt;Это текст '
+             'под спойлером&lt;/span&gt;:\n'
+             '<span class="tg-spoiler">Это текст под '
+             'спойлером</span>\n\n'
+             '&lt;tg-spoiler&gt;И это текст под '
+             'спойлером&lt;/tg-spoiler&gt;:\n'
+             '<tg-spoiler>И это текст под спойлером</tg-spoiler>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/link"
+@dp.message(Command(commands='link'))
+async def process_link_command(message: Message):
+    await message.answer(
+        text='&lt;a href="https://stepik.org/120924"&gt;Внешняя '
+             'ссылка&lt;/a&gt;:\n'
+             '<a href="https://stepik.org/120924">Внешняя ссылка</a>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/tglink"
+@dp.message(Command(commands='tglink'))
+async def process_tglink_command(message: Message):
+    await message.answer(
+        text='&lt;a href="tg://user?id=173901673"&gt;Внутренняя '
+             'ссылка&lt;/a&gt;:\n'
+             '<a href="tg://user?id=173901673">Внутренняя ссылка</a>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/code"
+@dp.message(Command(commands='code'))
+async def process_code_command(message: Message):
+    await message.answer(
+        text='&lt;code&gt;Это моноширинный текст&lt;/code&gt;:\n'
+             '<code>Это моноширинный текст</code>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/pre"
+@dp.message(Command(commands='pre'))
+async def process_pre_command(message: Message):
+    await message.answer(
+        text='&lt;pre&gt;Предварительно отформатированный '
+             'текст&lt;/pre&gt;:\n'
+             '<pre>Предварительно отформатированный текст</pre>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/precode"
+@dp.message(Command(commands='precode'))
+async def process_precode_command(message: Message):
+    await message.answer(
+        text='&lt;pre&gt;&lt;code class="language-'
+             'python"&gt;Предварительно отформатированный '
+             'блок кода на языке Python&lt;/code&gt;&lt;/pre&gt;:\n'
+             '<pre><code class="language-python">Предварительно '
+             'отформатированный блок кода на языке Python</code></pre>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/precodediff"
+@dp.message(Command(commands='precodediff'))
+async def process_precodediff_command(message: Message):
+    await message.answer(
+        text='С помощью этого текста можно лучше понять '
+             'разницу между тегами &lt;code&gt; и '
+             '&lt;pre&gt; - текст внутри '
+             'тегов &lt;code&gt; <code>не выделяется в '
+             'отдельный блок</code>, а становится '
+             'частью строки, внутрь которой помещен, в то время как '
+             'тег &lt;pre&gt; выделяет текст <pre>в отдельный '
+             'блок,</pre> разрывая '
+             'строку, внутрь которой помещен'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/boldi"
+@dp.message(Command(commands='boldi'))
+async def process_boldi_command(message: Message):
+    await message.answer(
+        text='&lt;b&gt;&lt;i&gt;Это жирный наклонный '
+             'текст&lt;/i&gt;&lt;/b&gt;:\n\n'
+             '<b><i>Это жирный наклонный текст</i></b>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/iu"
+@dp.message(Command(commands='iu'))
+async def process_iu_command(message: Message):
+    await message.answer(
+        text='&lt;i&gt;&lt;u&gt;Это наклонный подчеркнутый '
+             'текст&lt;/u&gt;&lt;/i&gt;:\n\n'
+             '<i><u>Это наклонный подчеркнутый текст</u></i>'
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/biu"
+@dp.message(Command(commands='biu'))
+async def process_biu_command(message: Message):
+    await message.answer(
+        text='&lt;b&gt;&lt;i&gt;&lt;u&gt;Это жирный '
+             'наклонный подчеркнутый '
+             'текст&lt;/u&gt;&lt;/i&gt;&lt;/b&gt;:\n\n'
+             '<b><i><u>Это жирный наклонный подчеркнутый '
+             'текст</u></i></b>'
     )
 
 
 # Этот хэндлер будет срабатывать на любые сообщения, кроме команд,
 # отлавливаемых хэндлерами выше
-@dp.message()
 async def send_echo(message: Message):
     await message.answer(
         text='Я даже представить себе не могу, '
